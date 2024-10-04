27cbfaae94e5f189921e38205789f475b1a624b0
claranceJ <jacktonec@gmail.com>
2024-10-04 18:40:21 +0300

    feat: Add routing to the comps

--- sandly/src/App.jsx
+++ sandly/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Home from "./components/Home"; // TODO We'll create this Home component for the landing page
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+   
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+              </ul>
+            </nav>
 
-export default App
+     
+    </Router>
+  );
+};
+
+export default App;
